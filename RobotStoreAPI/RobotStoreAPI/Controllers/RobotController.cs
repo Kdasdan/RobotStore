ba82dba1c111b1c0cf010c48734417216240c86a
Kdasdan <dasdankazim@gmail.com>
2022-03-09 18:45:51 +0100

    setup back-end

---
+++ RobotStoreAPI/RobotStoreAPI/Controllers/RobotController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RobotStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RobotController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Robot>>> Get()
+        {
+            var robots = new List<Robot>
+            {
+                new Robot {
+                    Id = 1,
+                    Name = "Robot",
+                    Price = 500
+                }
+            };
+
+            return Ok(robots);
+        }
+    }
+}
---
+++ RobotStoreAPI/RobotStoreAPI/Robot.cs
@@ -0,0 +1,9 @@
+﻿namespace RobotStoreAPI
+{
+    public class Robot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+    }
+}
